fe3032785a2c27804e02726d51558e4e6ec55b26
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-04-14 18:16:33 +0200

    simulazioni comprensive

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 class Movie 
 {
-    public $nome = 'titolo';
-    public $genere = 'sconosciuto' // sia titolo che sconosciuto sono valori di default che assegno con =
+    private $nome = 'titolo';
+    public $genere = 'sconosciuto'; // sia titolo che sconosciuto sono valori di default che assegno con =
+
+    function getName(){         //se setto su private un attributo per tirarlo fuori mi serve un metodo dentro la classe 
+        return $this->nome;     //non posso nemmeno leggerlo se e' private
+    }
 }
 
 $titanic = new Movie();
 $rambo = new Movie();
 $matrix = new Movie();
+ 
+var_dump($titanic->getName()); //non serve $ dopo la freccia
+//var_dump($titanic);
+//echo $titanic->name;
