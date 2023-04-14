f6b052077d8b0ed713b0f24a9a7b4dabdbf6d902
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-04-14 16:19:19 +0200

    creata struttura con 2 attributi e 3 istanze

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+class Movie 
+{
+    public $nome = 'titolo';
+    public $genere = 'sconosciuto' // sia titolo che sconosciuto sono valori di default che assegno con =
+}
+
+$titanic = new Movie();
+$rambo = new Movie();
+$matrix = new Movie();
